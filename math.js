3a3cb41284ed69b8be990a6b95cf1a2f610911ec
Aditee Jha <aditeejha2005@gmail.com>
2026-04-02 15:54:44 +0530

    changes

---
+++ math.js
@@ -0,0 +1,17 @@
+const sum(a,b)=>a+b;
+const sub(a,b)=>a-b;
+const mul(a,b)=>a*b;
+const div(a,b)=>a/b;
+const pi=3.14;
+const g=9.8;
+
+let obj={
+    sum:sum,
+    sub:sub,
+    mul:mul,
+    div:div,
+    pi:pi,
+    g:g
+};
+
+module.exports=obj;
