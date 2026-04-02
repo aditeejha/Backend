1b38696798b4e499d036dbe21289532fee2c5d53
Aditee Jha <aditeejha2005@gmail.com>
2026-04-02 15:55:36 +0530

    changes

--- script.js
+++ script.js
@@ -2,4 +2,11 @@
 // for(let i=0; i<n; i++){
 //     console.log("Hello");
 // }
-console.log(process.argv);
+//console.log(process.argv);
+const math=require('./math');
+console.log(math.sum(2,3));
+console.log(math.sub(5,2));
+console.log(math.mul(3,4));
+console.log(math.div(10,2));
+console.log(math.pi);
+console.log(math.g);
